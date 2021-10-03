99baa82926b50c227e4fa660d2070775b453c065
Tom Davison <tomwilliamdavison@gmail.com>
2021-10-03 17:18:04 +1100

    Added path + updates to truck controller

--- LudumDare49/Assets/Scripts/PlayerController.cs
+++ LudumDare49/Assets/Scripts/PlayerController.cs
@@ -28,12 +28,52 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform rearLeftWheelTransform;
     [SerializeField] private Transform rearRightWheelTransform;
 
+    private Rigidbody rb;
+    public float antiRollVal = 10000f;
+    
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass += new Vector3(0, -0.20f, 1.0f);
+    }
+
+    private void Update()
+    {
+    }
+
+    private void UpdateAntiRoll(WheelCollider leftWheel, WheelCollider rightWheel)
+    {
+        WheelHit hit;
+        float travelL=1.0f;
+        float travelR=1.0f;
+        bool groundedL = leftWheel.GetGroundHit(out hit);
+        if (groundedL){
+            travelL = (-leftWheel.transform.InverseTransformPoint(hit.point).y - leftWheel.radius) / leftWheel.suspensionDistance;
+        }
+        bool groundedR = rightWheel.GetGroundHit(out hit);
+        if (groundedR){
+            travelR = (-rightWheel.transform.InverseTransformPoint(hit.point).y - rightWheel.radius) / rightWheel.suspensionDistance;
+        }
+        
+        float antiRollForce = (travelL - travelR) * antiRollVal;
+        
+        if (groundedL)
+            rb.AddForceAtPosition(leftWheel.transform.up * -antiRollForce,
+                leftWheel.transform.position);  
+ 
+        if (groundedR)
+            rb.AddForceAtPosition(rightWheel.transform.up * antiRollForce,
+                rightWheel.transform.position);  
+    }
+
     private void FixedUpdate()
     {
         GetInput();
         HandleMotor();
         HandleSteering();
         UpdateWheels();
+        UpdateAntiRoll(frontLeftWheelCollider, frontRightWheelCollider);
+        UpdateAntiRoll(rearLeftWheelCollider, rearRightWheelCollider);
     }
 
 
@@ -54,8 +94,8 @@ public class PlayerController : MonoBehaviour
 
     private void ApplyBreaking()
     {
-        frontRightWheelCollider.brakeTorque = currentbreakForce;
-        frontLeftWheelCollider.brakeTorque = currentbreakForce;
+        //frontRightWheelCollider.brakeTorque = currentbreakForce;
+        //frontLeftWheelCollider.brakeTorque = currentbreakForce;
         rearLeftWheelCollider.brakeTorque = currentbreakForce;
         rearRightWheelCollider.brakeTorque = currentbreakForce;
     }
